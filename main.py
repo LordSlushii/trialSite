49308c3d26556ecb9d4945bfae8f0d1db769eb48
LordSlushii <88372623+LordSlushii@users.noreply.github.com>
2025-04-24 16:58:03 +0530

    Login Page

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        print("Username:", username)
+        print("Password:", password)
+        return f"Hello, {username}!"  # or redirect or render_template, etc.
+    return render_template("index.html")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-
